652dd6a497b45b4bd3851f708b0d38238f5aa450
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-08-16 21:24:18 +0900

    asda

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='finank',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # Choose an appropriate license
