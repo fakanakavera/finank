f12a7ff27c0f48c6702fa1425e3c6fcbad3a5747
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-08-16 21:26:43 +0900

    dsa

--- finank/views.py
+++ finank/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # make a view that returns a test page DO NOT USE HTML FILE, MAKE THE HTML IN THE VIEW
 def test(request):
-    return render(request, 'finank/test.html', {})
+    return render(request, 'test.html', {})
