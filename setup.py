5e604f425a620fb3b6fdffb22abb6111426b65b9
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-08-17 20:47:01 +0900

    39

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='finank',
-    version='0.9.37',
+    version='0.9.38',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # Choose an appropriate license
