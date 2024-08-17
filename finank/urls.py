20ebceeeb1c55bf9e08ea5029aca36351a40c86d
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-08-17 11:53:55 +0900

    5

--- finank/urls.py
+++ finank/urls.py
@@ -3,11 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # import test view from views.py
-from .views import test
+from .views import test, upload_receipt
 
 # create a path to the test view
 urlpatterns = [
     path('test/', test, name='test'),
+    path('upload-receipt/', upload_receipt, name='upload_receipt'),
 ]
 
 if settings.DEBUG:
