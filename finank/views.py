60603fe8d843a739bf29f5060c0bbc7cce4f8756
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2024-08-17 11:52:35 +0900

    3

--- finank/views.py
+++ finank/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Expense, Receipt
+
 # make a view that returns a test page DO NOT USE HTML FILE, MAKE THE HTML IN THE VIEW
 def test(request):
     return render(request, 'finank/test.html', {})
+
+def upload_receipt(request):
+    if request.method == 'POST':
+        expense_id = request.POST.get('selected_expense_id')
+        receipt_file = request.FILES.get('receipt')
+        expense = get_object_or_404(Expense, id=expense_id)
+
+        # Save the receipt linked to the selected expense
+        Receipt.objects.create(expense=expense, image=receipt_file)
+
+        # Redirect to a success page or back to the form
+        return redirect('upload_receipt')
+
+    # Retrieve all expenses (or a specific subset depending on your needs)
+    expenses = Expense.objects.all()
+    return render(request, 'upload_receipt.html', {'expenses': expenses})
